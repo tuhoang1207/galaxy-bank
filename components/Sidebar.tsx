21629761ed10a4d649b8a1412a08d7327dc22831
tuhoang1207 <luuhoangtu179@gmail.com>
2024-10-09 21:28:52 +0700

    finalize the app

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,6 +7,7 @@ import { sidebarLinks } from '@/constants'
 import { cn } from '@/lib/utils'
 import { usePathname } from 'next/navigation'
 import Footer from './Footer'
+import PlaidLink from './PlaidLink'
 
 const Sidebar = ({user}: SiderbarProps) => {
     const pathname = usePathname();
@@ -32,7 +33,7 @@ const Sidebar = ({user}: SiderbarProps) => {
                 )
             })}
 
-            USER
+            <PlaidLink user={user} />
         </nav>
 
         <Footer user={user}/>
